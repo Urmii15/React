8c868e5c3a42f7582a45cf339796af543650e1e2
urmii15 <urmilasoni6363@gmail.com>
2026-04-30 16:03:21 +0530

    multile added files

--- pro/src/App.jsx
+++ pro/src/App.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
+import Card from './components/Card';
+import User from './components/User';
 
 const App = () => {
+
+  // const arr = [1, 2, 3, 4, 5];
+
+  const arr = [{ user: 'Rohit' }, { user: 'Rohan' }, { user: 'Aman' }, { user: 'Sahil' }, { user: 'Shivam' }];
+
+   arr.map(function(elem) {
+    console.log(elem.user);
+  })
+
   return (
     <>
       <div className='parent'>
+        {/* <Card /> */}
 
-        <div className="card">
+        {/* <User name={arr[0]} />  
+        <User name='Rohan' />  
+        <User name='Aman' />   */}
 
-          <div className="top">
-            <img src="" alt="" />
-            <button>Save</button>
-          </div>
 
-          <div className="center">
+      </div> 
 
-          </div>
-          <div className="bottom">
+      {/* {arr.map((item) => {
+          return <User name={item} />
+        })} */}
 
-          </div> 
-           
-        </div>
 
-      </div>
     </>
   );
 }
